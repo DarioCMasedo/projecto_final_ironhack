f0a8fccbb671f5ad605c1e622b5c06fd66d182d3
DarioCMasedo <daconmas@gmail.com>
2021-05-16 17:52:48 +0200

    Subida streamlit

--- src/funciones.py
+++ src/funciones.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from mplsoccer.pitch import Pitch
+import matplotlib.pyplot as plt
 
 porteros = pd.read_csv('output/predict_posiciones/porteros.csv')
 defensas = pd.read_csv('output/predict_posiciones/defensas.csv')
@@ -31,3 +33,119 @@ def forwards(numero, valor_jugador,edad):
 
 
 # Alineaciones
+
+# 442
+def alineacion_442(valor_total,edad):
+    try:
+        valor_jugador = valor_total/11
+        porteros = goalkeeper(1,valor_jugador,edad).reset_index()
+        defensas = defenders(4,valor_jugador,edad).reset_index()
+        medios = midfielders(4,valor_jugador,edad).reset_index()
+        delanteros = forwards(2,valor_jugador,edad).reset_index()
+        pitch = Pitch(pitch_color='grass', line_color='white',stripe=True)
+        fig, ax = pitch.draw(figsize=(20, 20))
+        annotation = ax.annotate(porteros['short_name'][0], (5, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][0], (24, 30), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][1], (24, 52), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][2], (32, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][3], (32, 68), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][0], (60, 30), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][1], (60, 52), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][2], (60, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][3], (60, 68), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][0], (89, 24), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][1], (89, 60), fontsize=40, ha='center')
+        plt.savefig('output/images/442.jpg')
+        result = pd.concat([porteros,defensas,medios,delanteros]).reset_index()
+        result = result[['short_name','nationality','position', 'age','wage_eur','value_eur','value_pred']]
+        return result
+    except:
+        plt.savefig('output/images/442.jpg')
+        return "No se han encontrado jugadores con esos parametros"
+
+# 433
+def alineacion_433(valor_total,edad):
+    try:
+        valor_jugador = valor_total/11
+        porteros = goalkeeper(1,valor_jugador,edad).reset_index()
+        defensas = defenders(4,valor_jugador,edad).reset_index()
+        medios = midfielders(3,valor_jugador,edad).reset_index()
+        delanteros = forwards(3,valor_jugador,edad).reset_index()
+        pitch = Pitch(pitch_color='grass', line_color='white',stripe=True)
+        fig, ax = pitch.draw(figsize=(25, 25))
+        annotation = ax.annotate(porteros['short_name'][0], (5, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][0], (24, 30), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][1], (24, 52), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][2], (32, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][3], (32, 68), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][0], (60, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][1], (60, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][2], (89, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][0], (60, 68), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][1], (89, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][2], (89, 68), fontsize=40, ha='center')
+        plt.savefig('output/images/433.jpg')
+        result = pd.concat([porteros,defensas,medios,delanteros]).reset_index()
+        result = result[['short_name','nationality','position', 'age','wage_eur','value_eur','value_pred']]
+        return result
+    except:
+        plt.savefig('output/images/433.jpg')
+        return "No se han encontrado jugadores con esos parametros"
+
+# 343
+def alineacion_343(valor_total,edad):
+    try: 
+        valor_jugador = valor_total/11
+        porteros = goalkeeper(1,valor_jugador,edad).reset_index()
+        defensas = defenders(3,valor_jugador,edad).reset_index()
+        medios = midfielders(4,valor_jugador,edad).reset_index()
+        delanteros = forwards(3,valor_jugador,edad).reset_index()
+        pitch = Pitch(pitch_color='grass', line_color='white',stripe=True)
+        fig, ax = pitch.draw(figsize=(25, 25))
+        annotation = ax.annotate(porteros['short_name'][0], (5, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][0], (89, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][1], (24, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][2], (32, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][0], (32, 68), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][1], (60, 30), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][2], (60, 52), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][3], (60, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][0], (60, 68), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][1], (89, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][2], (89, 68), fontsize=40, ha='center')
+        plt.savefig('output/images/343.jpg')
+        result = pd.concat([porteros,defensas,medios,delanteros]).reset_index()
+        result = result[['short_name','nationality','position', 'age','wage_eur','value_eur','value_pred']]
+        return result
+    except:
+        plt.savefig('output/images/343.jpg')
+        return "No se han encontrado jugadores con esos parametros"
+# 532
+
+def alineacion_532(valor_total,edad):
+    try:
+        valor_jugador = valor_total/11
+        porteros = goalkeeper(1,valor_jugador,edad).reset_index()
+        defensas = defenders(5,valor_jugador,edad).reset_index()
+        medios = midfielders(3,valor_jugador,edad).reset_index()
+        delanteros = forwards(2,valor_jugador,edad).reset_index()
+        pitch = Pitch(pitch_color='grass', line_color='white',stripe=True)
+        fig, ax = pitch.draw(figsize=(25, 25))
+        annotation = ax.annotate(porteros['short_name'][0], (5, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][0], (24, 25), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][1], (24, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][2], (35, 8), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][3], (35, 72), fontsize=40, ha='center')
+        annotation = ax.annotate(defensas['short_name'][4], (24, 60), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][0], (60, 40), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][1], (60, 12), fontsize=40, ha='center')
+        annotation = ax.annotate(medios['short_name'][2], (60, 68), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][0], (89, 24), fontsize=40, ha='center')
+        annotation = ax.annotate(delanteros['short_name'][1], (89, 60), fontsize=40, ha='center')
+        plt.savefig('output/images/532.jpg')
+        result = pd.concat([porteros,defensas,medios,delanteros]).reset_index()
+        result = result[['short_name','nationality','position', 'age','wage_eur','value_eur','value_pred']]
+        return result
+    except:
+        plt.savefig('output/images/532.jpg')
+        return "No se han encontrado jugadores con esos parametros"
--- streamlit.py
+++ streamlit.py
@@ -2,29 +2,61 @@ import streamlit as st
 from PIL import Image
 from streamlit import uploaded_file_manager
 from streamlit.logger import update_formatter
-import src.manage_data as data
+import src.funciones as fun
 import plotly.express as px
 import pandas as pd
 import folium
 #from streamlit import folium_static
 import codecs
 import streamlit.components.v1 as components
+from mplsoccer.pitch import Pitch
+
+
+
+porteros = pd.read_csv('output/predict_posiciones/porteros.csv')
+defensas = pd.read_csv('output/predict_posiciones/defensas.csv')
+medios = pd.read_csv('output/predict_posiciones/medios.csv')
+delanteros = pd.read_csv('output/predict_posiciones/delanteros.csv')
 
 
 st.write("""
-hola mundo
+
+    Monta tu equipo
+
 """)
 
-# Cargar dataframe 
 
- # dataframe = carga_data()
+n = st.selectbox(
 
-# st.dataframe(data.carga_data())
+    "Selecciona una alineacion", ["442","433","343","532"]
 
-
-
-# Jugadores
-personaje = st.selectbox(
-
-    "Selecciona un jugador", data.lista_personajes()
 )
+
+
+#
+
+_input = st.text_input("Introduce tu presupuesto en millones de €")
+_input2 = st.text_input("Introduce edad maxima de los jugadores")
+
+if _input and _input2 and n == "442":
+    ax = fun.alineacion_442(int(_input),int(_input2))
+    image = Image.open('output/images/442.jpg')
+    st.write(ax)
+    st.image(image, caption='442')
+elif _input and _input2 and n == "433":
+    ax = fun.alineacion_433(int(_input),int(_input2))
+    image = Image.open('output/images/433.jpg')
+    st.write(ax)
+    st.image(image)
+elif _input and _input2 and n == "343":
+    ax = fun.alineacion_343(int(_input),int(_input2))
+    image = Image.open('output/images/343.jpg')
+    st.write(ax)
+    st.image(image, caption='343')
+elif _input and _input2 and n == "532":
+    ax = fun.alineacion_532(int(_input),int(_input2))
+    image = Image.open('output/images/532.jpg')
+    st.write(ax)
+    st.image(image, caption='532')
+else:
+    st.write('No Data')
