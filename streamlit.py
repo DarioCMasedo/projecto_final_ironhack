6146e9b562b075b5a5ce7e2231262a05d8aa6041
DarioCMasedo <daconmas@gmail.com>
2021-05-19 10:05:00 +0200

    Preparado para deployear streamlit

--- streamlit.py
+++ streamlit.py
@@ -13,10 +13,52 @@ from mplsoccer.pitch import Pitch
 
 
 
+
+
 porteros = pd.read_csv('output/predict_posiciones/porteros.csv')
 defensas = pd.read_csv('output/predict_posiciones/defensas.csv')
 medios = pd.read_csv('output/predict_posiciones/medios.csv')
 delanteros = pd.read_csv('output/predict_posiciones/delanteros.csv')
+
+
+
+imagen = Image.open("input/futbol.jpg")
+st.image(imagen)
+
+
+st.write("""
+
+    En este projecto veremos como hacer nuestra propia alineación seleccionando unos parámetros, y la prediccion del valor de cada jugador.
+    Lo primero de todo fue coger datos de unos 19 mil jugadores con sus respectivos atributos.
+    Lo siguiente que se necesita es saber que atributos son mejores para cada posicion que queremos, en mi caso seran 4, porteros, defensas, medios y delanteros.
+    
+
+""")
+
+atributos = st.selectbox(
+
+    "Atributos que hacen buenos a los jugadores por posicion", ["porteros","defensas","medios","delanteros"]
+
+)
+
+if atributos == "porteros":
+    image = Image.open("output/plot_atributos_posicion/porteros.png")
+    st.image(image)
+elif atributos == "defensas":
+    image = Image.open("output/plot_atributos_posicion/defensas.png")
+    st.image(image)
+elif atributos == "medios":
+    image = Image.open("output/plot_atributos_posicion/medios.png")
+    st.image(image)
+elif atributos == "delanteros":
+    image = Image.open("output/plot_atributos_posicion/delanteros.png")
+    st.image(image)
+
+st.write("""
+
+    Una vez ya visto que atributos son buenos para cada posicion, hago 4 modelos de prediccion con los atributos correspondientes.
+
+""")
 
 
 st.write("""
@@ -32,8 +74,6 @@ n = st.selectbox(
 
 )
 
-
-#
 
 _input = st.text_input("Introduce tu presupuesto en millones de €")
 _input2 = st.text_input("Introduce edad maxima de los jugadores")
